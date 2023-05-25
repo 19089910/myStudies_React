f4c13b8e97f18305aa70e9f9d37e407ef9a56873
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2023-05-25 14:07:53 -0400

    chore: add library styled-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+//import './styles.css' foi substituido pelo: 
+import { Conteiner, H1 } from './styles'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+function App () { 
+ const hello = "Hello React"  
+  return (  
+//  <div className='conteiner'> foi substituido pelo: 
+    <Conteiner> 
+      <H1>{hello} </H1>
+    </Conteiner> 
+//  </div> 
+  ); 
+} 
+ 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Conteiner = styled.div `
+    background-color: red;
+`
+
+export const H1 = styled.h1`
+    color: blue
+`
